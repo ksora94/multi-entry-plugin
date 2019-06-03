34ea4ae676d3b7ea6e1af185f28fbd4f99466c41
ksora <ksora@live.com>
2019-06-03 15:28:07 +0800

    feat: add exclude

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 const {dirname, resolve, basename, extname} = require('path');
 const DynamicEntryPlugin = require('webpack/lib/DynamicEntryPlugin');
 const globby = require('globby');
+const minimatch = require('minimatch');
 const _ = require('lodash');
 
 function getPureFileName(filename) {
   return basename(filename, extname(filename))
 }
 
-function getMultiEntry(entry) {
-  if (!entry) return {};
-  const srcDir = resolve(dirname(entry));
-  const ext = extname(entry).slice(1);
-  const getEntryKey =
-      (dir) => dirname(dir).slice(srcDir.length + 1) + '/' + getPureFileName(dir);
-
-  return globby(srcDir, {
-    deep: true,
-    expandDirectories: {
-      extensions: [ext]
-    }
-  }).then(paths => {
-    const entries = {};
-
-    paths.map(path => {
-      entries[getEntryKey(path)] = path;
-    });
-
-    return entries;
-  });
-}
-
 class MultiEntryPlugin {
   constructor(option = {}) {
     this.mainEntry = option.mainEntry || 'main';
+    this.exclude = option.exclude || [];
   }
 
   apply(compiler) {
@@ -41,10 +20,10 @@ class MultiEntryPlugin {
           cb => new DynamicEntryPlugin(context, cb).apply(compiler);
 
       if (typeof entry === 'string') {
-        createDynamicEntry(() => getMultiEntry(entry));
+        createDynamicEntry(() => this.getMultiEntry(entry));
       } else if (Array.isArray(entry)) {
         createDynamicEntry(
-            () => getMultiEntry(entry[0]).then(entries => {
+            () => this.getMultiEntry(entry[0]).then(entries => {
               const result = {...entries};
 
               entry.slice(1).forEach(
@@ -56,7 +35,7 @@ class MultiEntryPlugin {
         )
       } else if (typeof entry === 'object') {
         createDynamicEntry(
-            () => getMultiEntry(entry[this.mainEntry]).then(entries => {
+            () => this.getMultiEntry(entry[this.mainEntry]).then(entries => {
               return {
                 ...entry, ..._.omit(entries, ['/' + getPureFileName(entry[this.mainEntry])])
               }
@@ -68,6 +47,31 @@ class MultiEntryPlugin {
 
       return true;
     })
+  }
+
+  getMultiEntry(entry) {
+    if (!entry) return {};
+    const srcDir = resolve(dirname(entry));
+    const ext = extname(entry).slice(1);
+    const getEntryKey =
+        (dir) => dirname(dir).slice(srcDir.length + 1) + '/' + getPureFileName(dir);
+
+    return globby(srcDir, {
+      deep: true,
+      expandDirectories: {
+        extensions: [ext]
+      }
+    }).then(paths => {
+      const entries = {};
+
+      paths.map(path => {
+        if (this.exclude.every(e => !minimatch(path, resolve(srcDir, e)))) {
+          entries[getEntryKey(path)] = path;
+        }
+      });
+
+      return entries;
+    });
   }
 }
 
